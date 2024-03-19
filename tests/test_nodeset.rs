7996d51acc1dcdcfe472485bd35fbb8c424b10b7
Stefan Sommerfeld <stefan@qubecinema.com>
2024-03-19 13:31:49 +0100

    working nodeset test

--- tests/test_nodeset.rs
+++ tests/test_nodeset.rs
@@ -18,23 +18,34 @@ fn test_node_set() {
 
 #[test]
 fn test_node_set_with_children() {
-    let doc = Document::new().unwrap();
-    let mut root = Node::new("root", None, &doc).unwrap();
+    let mut doc = Document::new().unwrap();
+    let mut root = Node::new("LogReport", None, &doc).unwrap();
     let ns = Namespace::new(
         "lr",
         "http://www.smpte-ra.org/schemas/430-4/2008/LogRecord/",
         &mut root,
     )
     .unwrap();
+    Namespace::new(
+        "dcml",
+        "http://www.smpte-ra.org/schemas/433/2008/dcmlTypes/",
+        &mut root,
+    )
+    .unwrap();
+    Namespace::new("ds", "http://www.w3.org/2000/09/xmldsig#", &mut root).unwrap();
+    Namespace::new("xs", "http://www.w3.org/2001/XMLSchema", &mut root).unwrap();
+    Namespace::new(
+        "xsi",
+        "http://www.w3.org/2001/XMLSchema-instance",
+        &mut root,
+    )
+    .unwrap();
+    root.set_namespace(&ns).unwrap();
+    doc.set_root_element(&root);
+
     let mut node = Node::new("LogRecordBody", Some(ns.clone()), &doc).unwrap();
     root.add_child(&mut node).unwrap();
     let mut eventid = Node::new("EventID", Some(ns.clone()), &doc).unwrap();
-    eventid
-        .set_attribute(
-            "xmlns:lr",
-            "http://www.smpte-ra.org/schemas/430-4/2008/LogRecord/",
-        )
-        .unwrap();
     eventid
         .set_content("urn:uuid:1f9d3a08-edd6-4402-99c0-b80d9c7614fc")
         .unwrap();
@@ -49,7 +60,7 @@ fn test_node_set_with_children() {
     eventsubtype.set_content("SPBStartup").unwrap();
     node.add_child(&mut eventsubtype).unwrap();
 
-    println!("{:?}", doc.node_to_string(&node));
+    println!("{:?}", doc.to_string());
 
     let node_set = XmlSecNodeSet::get_children(&doc, &node, false, false);
 
@@ -67,10 +78,11 @@ fn test_node_set_with_children() {
     );
     trans_ctx.append(trans);
     let data = trans_ctx.xml_execute(&node_set).unwrap();
+    let base64_data = std::str::from_utf8(&data).unwrap();
 
-    let base64_data = BASE64_STANDARD.encode(data);
+    println!("{:?}", data);
     println!("{}", base64_data);
-    assert_eq!(data, b"");
+    assert_eq!(base64_data, "0HwjYW3B/l79oq2NHVctyN7qMhE=");
 }
 
 /*
